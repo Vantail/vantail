8976910ec5a20d81530a9d17f36b5a126dfccf02
Jeroen van Wissen <jeroen.van.wissen@gmail.com>
2026-08-25 19:44:58 +0200

    Release 0.1.1

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -24,7 +24,7 @@ export { bundle, type BundleInput, type BundleResult } from "./bundle/index.js";
 export { buildRuntimeConfig, type RuntimeConfigInput } from "./runtime-config.js";
 
 /** Kept in step with package.json by the release script. */
-export const VERSION = "0.1.0";
+export const VERSION = "0.1.1";
 
 /** `ConfigError` is thrown from a package the CLI does not re-export. */
 export const ConfigErrorName = "ConfigError";
